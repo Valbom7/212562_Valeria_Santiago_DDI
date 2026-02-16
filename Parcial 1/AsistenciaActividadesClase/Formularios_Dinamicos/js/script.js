a81371aede6e5857bc01489525555b46bb3930a3
Valeria Saenz <cinnamorollds@gmail.com>
2026-02-16 11:53:44 -0700

    [Agrega] Se actualiza orgnizacion de archivos y se actualiza actividad de formularios dinamicos

--- Parcial 1/AsistenciaActividadesClase/Formularios_Dinamicos/js/script.js
+++ Parcial 1/AsistenciaActividadesClase/Formularios_Dinamicos/js/script.js
@@ -17,6 +17,21 @@ boton.addEventListener("click",(e)=>{
     document.body.appendChild(apellido_info);
 })
 
+function cambiarNumero(event){
+    console.log(event.target.value);
+    const numeroElementos = event.target.value;
+    const contenido = document.querySelector("#contenedor_correos");
+    contenido.innerHTML = " ";
+    for(i=1; i<= event.target.value; i++){
+        
+         const htmlAgregar =  `<label for="correo-${i}">Ingrese el correo ${i}</label>
+            <input type="email" name="correo-${i}" id="correo-${i}">
+            <br></br>`;
+        contenido.innerHTML+= htmlAgregar;
+    }
+   
+}
+
 class Usuario{
     constructor(nom, ape){
 
